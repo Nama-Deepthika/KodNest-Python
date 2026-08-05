b8079d16bc16be76b661149f6bee5ab9335279e0
Nama-Deepthika <saideepthika63@gmail.com>
2026-08-05 11:14:38 +0530

    input type conversion

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/M02/T01/inputtype.py
@@ -0,0 +1,13 @@
+Student_name = input()
+Student_age = int(input())
+Course_rating = float(input())
+
+# Display the values
+print(f"Student: {Student_name}")
+print(f"Age: {Student_age}")
+print(f"Rating: {Course_rating}")
+
+# Display the data types
+print(type(Student_name))
+print(type(Student_age))
+print(type(Course_rating))
