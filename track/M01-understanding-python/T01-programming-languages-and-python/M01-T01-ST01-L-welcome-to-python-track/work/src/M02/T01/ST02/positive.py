a78c24a8f79f003b559b4b2be7f75cb94ed04aec
Nama-Deepthika <saideepthika63@gmail.com>
2026-08-05 15:19:06 +0530

    positive

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/M02/T01/ST02/positive.py
@@ -0,0 +1,11 @@
+# Read the number
+number = int(input())
+
+# Check the number is positive, negative or zero
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
+    
