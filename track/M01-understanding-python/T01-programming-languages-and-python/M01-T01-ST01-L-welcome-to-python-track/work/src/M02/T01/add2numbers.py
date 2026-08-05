30ff1d71dacc06800579c7ad10cd93b67e5187d4
Nama-Deepthika <saideepthika63@gmail.com>
2026-08-05 11:56:42 +0530

    adding two numbers

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/M02/T01/add2numbers.py
@@ -0,0 +1,8 @@
+# Read the numbers
+first_number = int(input("enter first number:"))
+second_number = int(input("enter second number:"))
+
+#Calculate the sum
+result = first_number + second_number
+sum_result = result
+print(f"Sum: {result}")
