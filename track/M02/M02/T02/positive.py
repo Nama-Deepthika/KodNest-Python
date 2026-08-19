0fd4a11c7aa9fe91c9f0a8e7092b6bb2556a5b85
Nama-Deepthika <saideepthika63@gmail.com>
2026-08-19 13:21:04 +0530

    check positive

---
+++ track/M02/M02/T02/positive.py
@@ -0,0 +1,10 @@
+# Read the number
+number = int(input())
+
+# Check wheather the number is positive, negative or zero
+if number > 0:
+    print("number is Positive")
+elif number < 0:
+    print("number is Negative")
+else:
+    print("number is Zero")
